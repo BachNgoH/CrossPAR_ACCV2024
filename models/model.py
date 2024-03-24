7805d4115d322dc022db15533751c0d3e99aba34
BachNgoH <nlmbao2015@gmail.com>
2024-03-25 00:51:21 +0700

    Add fusion

--- models/model.py
+++ models/model.py
@@ -11,11 +11,21 @@ class PARModel(nn.Module):
             self.backbone = swin_small_patch4_window7_224(img_size=(224, 224), drop_path_rate=0.1)
         if config["backbone"] == "x2vlm":
             self.backbone = load_pretrained_vision_tower(config["ckpt"], config)
+
+        if config["backbone"] == "fusion":
+            res = config["image_res"]
+            self.backbone_1 = swin_small_patch4_window7_224(img_size=(res, res), drop_path_rate=0.1)
+            self.backbone_2 = load_pretrained_vision_tower(config["ckpt"], config)
+
+            self.adapter_1 = nn.Linear(config['embed_dim'], 512)
+            self.adapter_2 = nn.Linear(config['embed_dim'], 512)
+            self.classifier = nn.Linear(512, config['num_attr'])
+
         else:
             self.backbone = timm.create_model(config["backbone"], pretrained=True, num_classes=512)        
-        
+            self.classifier = nn.Linear(config['embed_dim'], config["num_attr"])
+
         self.backbone_name = config["backbone"]
-        self.classifier = nn.Linear(config['embed_dim'], config["num_attr"])
 
     def forward(self, x):
         if self.backbone_name == "x2vlm":
